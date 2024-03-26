d929f1d125efc5134ff24b3be2254ba59c892e43
SCA260 <150054712+SCA260@users.noreply.github.com>
2024-03-26 11:42:57 +0100

    Create PlayerScoreValues.h

---
+++ R-Type/PlayerScoreValues.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SDL_ttf.h>
+
+int P1_Lives = 3;
+int P1_Score = 0;
+int Hi_Score = 174500;
+// Color for the text
+SDL_Color textColor = { 255, 255, 255, 255 }; // White color
+
+// Surface for the score text
+SDL_Surface* scoreSurface = NULL;
+
+// Texture for the score text
+SDL_Texture* scoreTexture = NULL;

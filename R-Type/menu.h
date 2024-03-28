ddd6f9290b326e1eebbed249359ff9a9736f6ffa
Lynn <jacquelinehuynh@outlook.fr>
2024-03-28 14:47:13 +0100

    menu
    in progress

---
+++ R-Type/menu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void Menu(SDL_Renderer* renderer,
+	SDL_Texture* backgroundTexture,
+	SDL_Texture* startButtonTexture,
+	SDL_Texture* optionButtonTexture,
+	SDL_Texture* quitButtonTexture);

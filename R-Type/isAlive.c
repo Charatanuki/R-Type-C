8e7e54ce440ffa273d1ecd7be8943ca06ab4b4a6
SCA260 <150054712+SCA260@users.noreply.github.com>
2024-03-29 11:53:24 +0100

    Update isAlive.c

--- R-Type/isAlive.c
+++ R-Type/isAlive.c
@@ -7,7 +7,7 @@ bool isAliveCheck(Player* player) {
 	if (player->health > 0)
 		return true;
 	printf("GameOver \n");
-	printf(" Kills : %d \n Score : %d\n Level: %d \n ",P1_Score / 200, P1_Score, P1_Score / 3500 +1);
+	printf(" Kills : %d \n Score : %d\n Level: %d \n ",P1_Score / 200, P1_Score, level);
 	//SDL_Quit(); //si l'on souhaite quitter le jeu directement après
 	return false;
 }
